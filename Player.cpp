1b31ac4cf10381d6e9e55b77c5dd1f5afe0993fa
brubachr <brubachr@mcmaster.ca>
2023-11-22 15:12:47 -0500

    player object done

--- Player.cpp
+++ Player.cpp
@@ -18,15 +18,75 @@ Player::~Player()
 void Player::getPlayerPos(objPos &returnPos)
 {
     // return the reference to the playerPos arrray list
+    returnPos.x = playerPos.x;
+    returnPos.y = playerPos.y;
+    returnPos.symbol = playerPos.symbol;
 }
 
 void Player::updatePlayerDir()
 {
-    // PPA3 input processing logic        
+    // PPA3 input processing logic
+    switch(mainGameMechsRef->getInput())
+    {                      
+        case 'w':
+            if (myDir == LEFT || myDir == RIGHT || myDir == STOP){
+                myDir = UP;
+            }
+            break;
+        
+        case 's':
+            if (myDir == LEFT || myDir == RIGHT || myDir == STOP){
+                myDir = DOWN;
+            }
+            break;
+                     
+        case 'a':
+            if (myDir == UP || myDir == DOWN || myDir == STOP){
+                myDir = LEFT;
+            }
+            break;
+        
+        case 'd':
+            if (myDir == UP || myDir == DOWN || myDir == STOP){
+                myDir = RIGHT;
+            }
+            break;
+        
+        default:
+            break;
+    }
 }
 
 void Player::movePlayer()
 {
     // PPA3 Finite State Machine logic
+    switch(myDir){
+        case UP:
+            playerPos.y--;
+            break;
+        case DOWN:
+            playerPos.y++;
+            break;
+        case LEFT:
+            playerPos.x--;
+            break;
+        case RIGHT:
+            playerPos.x++;
+            break;
+        default:
+            break;
+    }
+    if (playerPos.y == 0){
+            playerPos.y = mainGameMechsRef->getBoardSizeY() - 2;
+        }
+    if (playerPos.y == mainGameMechsRef->getBoardSizeY() - 1){
+        playerPos.y = 1;
+    }
+    if (playerPos.x == 0){
+        playerPos.x = mainGameMechsRef->getBoardSizeX() - 2;
+    }
+    if (playerPos.x == mainGameMechsRef->getBoardSizeX() - 1){
+        playerPos.x = 1;
+    }
 }
 
